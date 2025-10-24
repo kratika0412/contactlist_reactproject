7428506473581c78199da65ac931cceffa1caaa9
kratika0412 <kratikajasuja@gmail.com>
2025-10-24 22:08:29 +0530

    Fix logo image

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ export default function Navbar({ onOpen }) {
     <header className="navbar">
       <div className="nav-inner">
         <div className="nav-left">
-          <img src="/src/assets/contactlistlogo.png" className="nav-logo" alt="Logo" />
+          <img src="/contactlistlogo.png" className="nav-logo" alt="Logo" />
         </div>
         {/* <h1 className="brand">Contact List</h1> */}
         <div className="nav-right" />
